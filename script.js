d28cea4935e4b55e59d004c7d82541c18e93e8fd
Vinglish <khan.ildar94@gmail.com>
2018-02-05 10:08:35 +0200

    while

---
+++ script.js
@@ -0,0 +1,24 @@
+for(var i = 1; i < 100; i ++) {
+	if (i % 3 == 0){
+		document.write(i + "<br />")
+	}
+	
+}
+
+
+
+/*var sum = 0;
+
+var i = 1;
+while(i <= 100){
+	sum += i;
+	i++;
+}
+alert(sum);*/
+
+
+
+
+
+
+
